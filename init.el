c7ad6c436a6c790d124ac57a56b57f44a7d52050
Akira Hayashi <rindrics@gmail.com>
2025-01-02 09:35:07 +0900

    Set font

--- init.el
+++ init.el
@@ -153,6 +153,56 @@
   (completion-styles . '(orderless))
   (completion-category-defaults . nil))
 
+;; Font Size checker
+;;
+;; |∞≤≥∏∑∫ ×±⊆⊇|
+;; |αβγδεζ ηθικλu|
+;; |abcdef ghijkl|
+;; |ABCDEF GHIJKL|
+;; |'";:-+ =/\~`?|
+;; |日本語 の美観|
+;; |あいう えおか|
+;; |アイウ エオカ|
+;; |ｱｲｳｴｵｶ ｷｸｹｺｻｼ|
+;;
+;; | hoge                 | hogeghoe | age               |
+;; |----------------------+----------+-------------------|
+;; | 今日もいい天気ですね | お、     | 等幅になった :+1: |
+
+
+(leaf font
+ :doc "Font settings with precise Unicode ranges"
+ :if window-system
+ :preface
+ (defun set-japanese-font (family)
+   (set-fontset-font (frame-parameter nil 'font) 'japanese-jisx0208 (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) 'japanese-jisx0212 (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) 'katakana-jisx0201 (font-spec :family family)))
+
+ (defun set-latin-and-greek-font (family)
+   (set-fontset-font (frame-parameter nil 'font) '(#x0250 . #x02AF) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x00A0 . #x00FF) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x0100 . #x017F) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x0180 . #x024F) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x2018 . #x2019) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x2588 . #x2588) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x2500 . #x2500) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x2504 . #x257F) (font-spec :family family))
+   (set-fontset-font (frame-parameter nil 'font) '(#x0370 . #x03FF) (font-spec :family family)))
+ :custom
+ (use-default-font-for-symbols . nil)
+ (inhibit-compacting-font-caches . t)
+ (jp-font-family . "SF Mono Square")
+ (default-font-family . "Hack Nerd Font")
+ :config
+ (when (eq system-type 'darwin)
+   (set-face-attribute 'default nil :family jp-font-family :height 140))
+ (set-japanese-font jp-font-family)
+ (set-frame-font jp-font-family nil t)
+ (set-latin-and-greek-font default-font-family)
+ (add-to-list 'face-font-rescale-alist (cons default-font-family 0.86))
+ (add-to-list 'face-font-rescale-alist (cons jp-font-family 1.0)))
+
 ;; -----------------------------------------------------------------------------------------
 ;;
 ;; MacOS
