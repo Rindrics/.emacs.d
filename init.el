50325314205007da797eb0caafeec808cc5b2663
Akira Hayashi <rindrics@gmail.com>
2025-04-09 02:41:12 +0900

    Use multiple cursors

--- init.el
+++ init.el
@@ -440,6 +440,36 @@
   :custom (org-bullets-bullet-list . '("" "" "" "" "" "" "" "" "" ""))
   :hook (org-mode-hook . (lambda () (org-bullets-mode 1))))
 
+(leaf multiple-cursors
+  :ensure t
+  :require t)
+
+(global-set-key (kbd "C-c m") 'hydra-multiple-cursors/body)
+(defhydra hydra-multiple-cursors (:color blue :hint nil)
+  "
+ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search
+ [Click] Cursor at point       [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil))
+
 ;; (profiler-report)
 ;; (profiler-stop)
 
